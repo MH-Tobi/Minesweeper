a48478d3f2c970138af623f79349a8edab738cdf
MH-Tobi <tobias_pflug@gmx.de>
2025-11-18 22:55:52 +0100

    cleanup Field-Class-Files

--- Field.cpp
+++ Field.cpp
@@ -5,23 +5,31 @@
 #include "MainWindow.h"
 
 
-Field::Field(uint16_t id, uint8_t size, QWidget *parent) : QPushButton(parent), _FieldID(id), _FieldSize(size), _FieldnearMines(-1), _FieldIsMarked(false), _FieldIsQuestionable(false)
+Field::Field(uint16_t id, uint8_t size, QWidget *parent) :
+    QPushButton(parent),
+    _FieldID(id),
+    _FieldSize(size),
+    _FieldisMine(false),
+    _FieldnearMines(-1),
+    _FieldIsQuestionable(false),
+    _FieldIsMarked(false)    
 {
     connect(this, &Field::rightClicked, this, &Field::setFieldIcon);
 }
 
-Field::~Field()
-{}
+Field::~Field(){}
 
-uint16_t Field::getFieldID()        {return _FieldID;}
-uint8_t  Field::getFieldSize()      {return _FieldSize;}
-bool     Field::getFieldIsMine()    {return _FieldisMine;}
-int8_t   Field::getFieldNearMines() {return _FieldnearMines;}
+uint16_t Field::getFieldID()            {return _FieldID;}
+void     Field::setFieldID(uint16_t id) {_FieldID = id;}
 
-void Field::setFieldID(uint16_t id)               {_FieldID = id;}
-void Field::setFieldSize(uint8_t size)            {_FieldSize = size;}
-void Field::setFieldIsMine(bool is_mine)          {_FieldisMine = is_mine;}
-void Field::setFieldNearMines(int8_t near_mines)  {_FieldnearMines = near_mines;}
+uint8_t Field::getFieldSize()             {return _FieldSize;}
+void    Field::setFieldSize(uint8_t size) {_FieldSize = size;}
+
+bool Field::getFieldIsMine()             {return _FieldisMine;}
+void Field::setFieldIsMine(bool is_mine) {_FieldisMine = is_mine;}
+
+int8_t Field::getFieldNearMines()                  {return _FieldnearMines;}
+void   Field::setFieldNearMines(int8_t near_mines) {_FieldnearMines = near_mines;}
 
 bool Field::getFieldIsQuestionable() {return _FieldIsQuestionable;};
 bool Field::getFieldIsMarked()       {return _FieldIsMarked;};
@@ -45,11 +53,13 @@ void Field::setFieldIcon()
         
         setIcon(QIcon("D:\\Projekte\\Minesweeper\\icons\\questionmark.png"));
         setIconSize(QSize(_FieldSize*3/4,_FieldSize*3/4));
+
     }else if (_FieldIsQuestionable)
     {
         _FieldIsQuestionable = false;
 
         setIcon(QIcon());
+        
     }else{
         _FieldIsMarked = true;
         
--- Field.h
+++ Field.h
@@ -13,26 +13,25 @@ class Field : public QPushButton
         ~Field();
 
         uint16_t getFieldID();
-        void setFieldID(uint16_t id);
+        void     setFieldID(uint16_t id);
 
         uint8_t getFieldSize();
-        void setFieldSize(uint8_t size);
+        void    setFieldSize(uint8_t size);
 
         bool getFieldIsMine();
         void setFieldIsMine(bool is_mine);
 
         int8_t getFieldNearMines();
-        void setFieldNearMines(int8_t near_mines);
+        void   setFieldNearMines(int8_t near_mines);
 
         bool getFieldIsQuestionable();
         bool getFieldIsMarked();
 
-        uint16_t _FieldID;
-        
     private:
-        uint8_t _FieldSize;
-        bool _FieldisMine;
-        int8_t _FieldnearMines;
+        uint16_t _FieldID;
+        uint8_t  _FieldSize;
+        bool     _FieldisMine;
+        int8_t   _FieldnearMines;
 
         bool _FieldIsQuestionable;
         bool _FieldIsMarked;
